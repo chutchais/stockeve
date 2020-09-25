633cf7af1e14aaebee2a618d6f34779c006d7c89
chutchais <chutchai.s@gmail.com>
2020-09-25 23:12:55 +0700

    add query by saleorder

--- winspeed/app.py
+++ winspeed/app.py
@@ -92,16 +92,54 @@ def fetch_sale_by_date(day):
 	return response
 	# return json.dumps(jdata, indent=4,sort_keys=True) ,200
 
+@app.route('/api/saleorder/<soid>', methods=['GET'])
+def fetch_saleorder(soid):
+	try:
+		conn = connect_db()
+		cur = conn.cursor()
+		sql = f"select s.SOInvID,s.GoodID,s.GoodName,s.GoodQty2,s.GoodAmnt,e.GoodCode from [dbwins_EMG].[dbo].[SOInvDT] s inner join [dbwins_EMG].[dbo].[EMGood] e on s.GoodID = e.GoodID where s.soinvid={soid}"
+		rows = fetch_data(sql,cur)
+		items =[]
+		for row in rows:
+			row_json = {
+				'SOInvID': row.SOInvID,
+				'GoodCode':row.GoodCode,
+				'GoodID':row.GoodID,
+				'GoodName':row.GoodName,
+				'GoodQty2':str(row.GoodQty2),
+				'GoodAmnt': str(row.GoodAmnt)
+			}
+			items.append(row_json)
+		jdata ={
+			"sql" : sql,
+			"rows" : len(rows),
+			"status":f"Fetch sale order {soid} is sucessful",
+			"items": items}
+		cur.close()
+		conn.close()
+	except Exception as e :
+		jdata ={
+			"sql" : sql,
+			"status":f"Unable to connect database : {e}"
+			}
+	# response=jsonify(jdata)
+	# response.headers.add('Access-Control-Allow-Origin', '*')
+	# return response
+	return json.dumps(jdata, indent=4,ensure_ascii=False).encode('utf8') ,200
+
 @app.route('/api/invoice/<invoice>', methods=['GET'])
 def fetch_invoice(invoice):
 	try:
 		conn = connect_db()
 		cur = conn.cursor()
-		sql = f"select s.SOInvID,s.GoodID,s.GoodName,s.GoodQty2,s.GoodAmnt,e.GoodCode from [dbwins_EMG].[dbo].[SOInvDT] s inner join [dbwins_EMG].[dbo].[EMGood] e on s.GoodID = e.GoodID where s.soinvid={invoice}"
+		# sql = f"select s.SOInvID,s.GoodID,s.GoodName,s.GoodQty2,s.GoodAmnt,e.GoodCode from [dbwins_EMG].[dbo].[SOInvDT] s inner join [dbwins_EMG].[dbo].[EMGood] e on s.GoodID = e.GoodID where s.soinvid={invoice}"
+		sql = f"SELECT h.DocuNo,d.SOInvID,e.goodid,e.GoodCode,d.GoodName,d.GoodQty2,d.GoodAmnt FROM [dbwins_EMG].[dbo].[SOInvHD] h inner join [dbwins_EMG].[dbo].[SOInvDT] d on h.SOInvID = d.SOInvID inner join [dbwins_EMG].[dbo].[EMGood] e on d.GoodID = e.GoodID where h.DocuNo='{invoice}'" 
+
 		rows = fetch_data(sql,cur)
 		items =[]
 		for row in rows:
 			row_json = {
+				'DocuNo':row.DocuNo,
 				'SOInvID': row.SOInvID,
 				'GoodCode':row.GoodCode,
 				'GoodID':row.GoodID,
