fcbabf7925fe2511dfb09348dbdf5732759ce57d
chutchais <chutchai.s@gmail.com>
2021-01-20 15:24:24 +0700

    Finish Transfer job task

--- src/transfer/tasks.py
+++ src/transfer/tasks.py
@@ -1,5 +1,7 @@
 import requests
 from transfer.models import ICStockHD,ICStockDetail
+from product.models import Product,ProductStock
+from store.models import Store
 
 def pull_transfer_winspeed():
     import datetime, pytz
@@ -8,7 +10,7 @@ def pull_transfer_winspeed():
     local_dt = datetime.datetime.now(tz=tz)
     # Print the local datetime
     # Added on Jan 8,2021 -- To increase 1 day for get sale data.
-    # local_dt = local_dt + datetime.timedelta(days=1)
+    local_dt = local_dt + datetime.timedelta(days=1)
     # ----------------------------------------------------------
     report_str = local_dt.strftime('%Y-%m-%d')
     print(f'Report time : {report_str}')
@@ -17,7 +19,7 @@ def pull_transfer_winspeed():
 def download_transfer(date='2020-09-21'):
     from datetime import datetime
     URL_TRANSFER = f'http://180.183.250.150:8081/api/transfer/date/{date}'
-    # URL_SALE = f'http://192.168.101.10:8081/api/sale/date/{date}'
+    URL_SALE = f'http://192.168.101.10:8081/api/sale/transfer/{date}'
     res = requests.get(URL_TRANSFER)
     for item in res.json()['pos']:
         # print(item)
@@ -43,7 +45,7 @@ def download_transfer(date='2020-09-21'):
 def download_transfer_items(trans_obj,transferdate):
     from datetime import datetime
     URL_TRANSFER_ITEM = f'http://180.183.250.150:8081/api/tranferdetail/{trans_obj.docuid}'
-    # URL_SALE_ITEM = f'http://192.168.101.10:8081/api/saleorder/{soinv_obj.soinvid}'
+    URL_SALE_ITEM = f'http://192.168.101.10:8081/api/tranferdetail/{trans_obj.docuid}'
     print (URL_TRANSFER_ITEM)
     res = requests.get(URL_TRANSFER_ITEM)
     for item in res.json()['items']:
@@ -70,37 +72,68 @@ def download_transfer_items(trans_obj,transferdate):
                     'created':transferdate
                     },
         )
-        print(f'Order ddate {transferdate}')
-        print(f'Create order detail {transferdate}')
+        # print(f'Order ddate {transferdate}')
+        # print(f'Create order detail {transferdate}')
         # Added on Sep 28,2020 -- To Pull Order detail
         # http://180.183.250.150:8081/api/saleorder/99551
         # http://192.168.101.10:8081/api/saleorder/99551
         if created:
-            pass
+            # pass
+            # Start to adjust balance in Stock
+            # 1)Get or Create Store object
+            store,created = Store.objects.get_or_create(
+                        name = invecode,
+                        defaults ={
+                            'title':invename,
+                            'sale_able':True}
+                        )
+            # 2) Get or Create Product
+            product,created = Product.objects.get_or_create(
+                        number=goodcode,
+                        defaults={
+                            'finished_goods': True,
+                            'title':goodname,
+                            'description':goodname}
+                        )
+            # 3) Adjust Number in Store
+            productstock,created = ProductStock.objects.get_or_create(
+                        store = store,product=product,
+                        defaults={
+                            'qty' : float(goodqty)
+                        }
+                        )
+            
+            if not created:
+                # Stock is exist -- need to adjust number
+                print(f'Adjust stock of {productstock} by {float(goodqty)}')
+                productstock.qty = productstock.qty + float(goodqty)
+                productstock.save()
+            else :
+                print(f'Creagte stock of {productstock} by {float(goodqty)}')
 
 
-from product.models import Product
-from store.models import Store
+# from product.models import Product
+# from store.models import Store
 
-def add_to_sale(product_code,store_code,saledate,qty=0,price=0,description=''):
-    print(f'Add Sale : {product_code} ,{store_code} ,{qty} ,{price} ,{description}')
-    # Verify Product
-    product,created = Product.objects.get_or_create(
-                            number=product_code,
-                            defaults={
-                                'finished_goods': True,
-                                'title':description,
-                                'description':description}
-                            )
-    # Verify Store
-    store,created = Store.objects.get_or_create(
-                            name = store_code,
-                            defaults ={
-                                'title':store_code,
-                                'sale_able':True}
-                            )
+# def add_to_sale(product_code,store_code,saledate,qty=0,price=0,description=''):
+#     print(f'Add Sale : {product_code} ,{store_code} ,{qty} ,{price} ,{description}')
+#     # Verify Product
+#     product,created = Product.objects.get_or_create(
+#                             number=product_code,
+#                             defaults={
+#                                 'finished_goods': True,
+#                                 'title':description,
+#                                 'description':description}
+#                             )
+#     # Verify Store
+#     store,created = Store.objects.get_or_create(
+#                             name = store_code,
+#                             defaults ={
+#                                 'title':store_code,
+#                                 'sale_able':True}
+#                             )
     
-    sale = Sale.objects.create(product=product,
-                    store=store,qty=qty,
-                    price=price,description=description,created=saledate,saledate=saledate)
-    print('Save Sale successful..')
+#     sale = Sale.objects.create(product=product,
+#                     store=store,qty=qty,
+#                     price=price,description=description,created=saledate,saledate=saledate)
+#     print('Save Sale successful..')
